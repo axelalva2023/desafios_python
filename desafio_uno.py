2483482e644f8c4c695bd3c71b69190110007a57
tecno420 <75450267+tecno420@users.noreply.github.com>
2022-02-24 17:33:06 -0300

    Add files via upload

---
+++ desafio_uno.py
@@ -0,0 +1,11 @@
+#Autor: Axel Alva
+#Primer desafio de python, practicando las salidas por pantalla y usando algunos comandos.
+print("Nombre","Apellido","Edad","email",sep = '\t')
+print("------","--------","----","-----",sep = '\t')
+print("Juan","Perez","23","juancito@hotmail.com",sep = '\t')
+print("Carlos","Calvo","24","carlitos@hotmail.com",sep = '\t')
+print("Federico","Ameal","28","federojo@hotmail.com",sep = '\t')
+print("Patricia","Juarez","58","patriciamonica@gmail.com",sep = '\t')
+print("Gustavo","Vera","34","gustavovera@gmail.com",sep = '\t')
+
+
